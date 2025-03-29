230859c0bac1aef832e6025c3056914d7aae204d
loc <loc08122004@gmail.com>
2025-03-29 16:53:26 +0700

    thêm menu...

--- fitfusion_frontend/lib/widgets/tabbar.dart
+++ fitfusion_frontend/lib/widgets/tabbar.dart
@@ -1,15 +1,89 @@
+import 'package:fitfusion_frontend/screen/intro.dart';
+import 'package:fitfusion_frontend/screen/profile.dart';
 import 'package:flutter/material.dart';
 import '../theme/theme.dart'; // Import theme nếu cần
 
 class AppBarCustom extends StatelessWidget {
   final VoidCallback? onBackPressed;
-  final VoidCallback? onMenuPressed;
 
   const AppBarCustom({
     Key? key,
     this.onBackPressed,
-    this.onMenuPressed,
   }) : super(key: key);
+  void _showMenu(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Stack(
+          children: [
+            Positioned(
+              top: 70, // Điều chỉnh vị trí dọc
+              right: 10, // Điều chỉnh vị trí ngang
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  width: 220, // Điều chỉnh kích thước
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _buildMenuItem( context,Icons.person, "Cơ bản", "Thông tin cá nhân",
+                      onTap: () {
+                          Navigator.pop(context); // Đóng menu trước
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => setProfile()), // Chuyển đến màn hình IntroApp
+                          );
+                        },
+                      ),
+                      Divider(),
+                      _buildMenuItem( context,Icons.help, "Trợ giúp","Yêu cầu trợ giúp"),
+                      Divider(),
+                      _buildMenuItem( context,Icons.info,"Giới thiệu",  "Về chúng tôi"),
+                      Divider(),
+                      _buildMenuItem(
+                        context,
+                        Icons.account_circle,
+                        "Tài Khoản",
+                        "Đăng xuất",
+                        onTap: () {
+                          Navigator.pop(context); // Đóng menu trước
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => IntroApp()), // Chuyển đến IntroApp
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+ Widget _buildMenuItem(BuildContext context, IconData icon, String title, String subtitle, {VoidCallback? onTap}) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black),
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: subtitle.isNotEmpty ? Text(subtitle, style: TextStyle(fontSize: 12)) : null,
+      onTap: onTap ?? () {}, // Nếu có onTap thì dùng, không thì để trống
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +91,20 @@ class AppBarCustom extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(
-          icon: const Icon(Icons.chevron_left, color: Colors.white,size: 50,),
+          icon: const Icon(Icons.chevron_left, color: Colors.white, size: 50),
           onPressed: onBackPressed ?? () => Navigator.of(context).pop(),
         ),
         const Text(
           "FITFUSION",
-          style: AppTextStyles.title,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
         IconButton(
-          icon: const Icon(Icons.menu, color: Colors.white, size: 50,),
-          onPressed: onMenuPressed ?? () {},
+          icon: const Icon(Icons.menu, color: Colors.white, size: 50),
+          onPressed: () => _showMenu(context),
         ),
       ],
     );
@@ -35,14 +113,101 @@ class AppBarCustom extends StatelessWidget {
 class AppBarCustomHeader extends StatelessWidget {
   final String fullname;
   final VoidCallback? onBackPressed;
-  final VoidCallback? onMenuPressed;
 
   const AppBarCustomHeader({
     super.key,
     required this.fullname,
     this.onBackPressed,
-    this.onMenuPressed,
   });
+
+  void _showMenu(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Stack(
+          children: [
+            Positioned(
+              top: 70,
+              right: 10,
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  width: 220,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _buildMenuItem(context, 
+                      Icons.person, 
+                      "Cơ bản", 
+                      "Thông tin cá nhân",
+                       onTap: () {
+                          Navigator.pop(context); // Đóng menu trước
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => IntroApp()), // Chuyển đến màn hình IntroApp
+                          );
+                        },
+                      ),
+                      Divider(),
+                      _buildMenuItem(context, Icons.help, "Trợ giúp", "Yêu cầu trợ giúp"),
+                      Divider(),
+                      _buildMenuItem(
+                      context,
+                      Icons.info,
+                      "Giới thiệu",  
+                      "Về chúng tôi",
+                      onTap: () {
+                          Navigator.pop(context); // Đóng menu trước
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => setProfile()), // Chuyển đến màn hình IntroApp
+                          );
+                        },
+                      ),
+                      Divider(),
+                      _buildMenuItem(
+                        context,
+                        Icons.account_circle,
+                        "Tài Khoản",
+                        "Đăng xuất",
+                        onTap: () {
+                          Navigator.pop(context); // Đóng menu trước
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => IntroApp()), // Chuyển đến màn hình IntroApp
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  Widget _buildMenuItem(BuildContext context, IconData icon, String title, String subtitle, {VoidCallback? onTap}) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black),
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: subtitle.isNotEmpty ? Text(subtitle, style: TextStyle(fontSize: 12)) : null,
+      onTap: onTap ?? () {},
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,12 +225,12 @@ class AppBarCustomHeader extends StatelessWidget {
               style: AppTextStyles.title,
             ),
             IconButton(
-              icon: const Icon(Icons.menu, color: Colors.white, size: 50,),
-              onPressed: onMenuPressed ?? () {},
+              icon: const Icon(Icons.menu, color: Colors.white, size: 50),
+              onPressed: () => _showMenu(context),
             ),
           ],
         ),
-        const SizedBox(height: 10), // Thêm khoảng cách giữa Row và Text.rich
+        const SizedBox(height: 10),
         Text.rich(
           TextSpan(
             children: [
