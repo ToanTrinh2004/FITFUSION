363f7fc1aba5997cd044b03964345ac8b6bbf493
ToanTrinh2004 <2251120326@ut.edu.vn>
2025-05-08 21:45:55 +0700

    Fetch User Info

--- fitfusion_backend/routers/user.router.js
+++ fitfusion_backend/routers/user.router.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 const userController = require('../controller/user.controller');
+const { verifyToken } = require('../middleware/auth.middleware');
 
-// User Authentication
+// Auth Routes
 router.post('/registration', userController.register);
 router.post('/login', userController.login);
 
 // User Info Routes
-router.post('/userInfo/create', userController.createUserInfo);
-router.put('/userInfo/update/:userId', userController.updateUserInfo); // Fixed missing '/'
-router.get('/userInfo/getUserInfo', userController.getUserInfo);
+router.post('/userinfo/create', verifyToken, userController.createUserInfo);
+router.put('/userinfo/update', verifyToken, userController.updateUserInfo);
+router.get('/userinfo', verifyToken, userController.getUserInfo);
 
+// User CRUD Routes (Admin or self-management)
+router.get('/users', verifyToken, userController.getUsers);         // Get all users
+router.get('/user', verifyToken, userController.getUser);           // Get current user
+router.put('/user/update', verifyToken, userController.updateUser); // Update current user
+router.delete('/user/delete', verifyToken, userController.deleteUser); // Delete current user
 
 module.exports = router;
